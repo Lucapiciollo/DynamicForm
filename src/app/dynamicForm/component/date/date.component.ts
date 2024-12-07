9239c5fc459258be1fa420f8aab4e76dbc4d55fb
Luca <lucapiciollo@gmail.com>
2024-12-07 13:21:48 +0100

    fix

--- src/app/dynamicForm/component/date/date.component.ts
+++ src/app/dynamicForm/component/date/date.component.ts
@@ -24,68 +24,17 @@ export class DateComponent extends BaseComponent {
 
   /************************************************************************************************************************************************************************ */
 
-
-  private _datarangeinput: MatDatepicker<Date>;
-
- 
-  @ViewChild("datepicker") set datarangeinput(datarangeinput: MatDatepicker<Date>) {
-    // if (!this.obs) {
-    //   this.obs = new Subscription();
-    // }
-    // if (datarangeinput) {
-    //   this._datarangeinput = datarangeinput;
-    // }
-  };
-
+  openedStream(eve, datainput: MatInput) { }
+  /************************************************************************************************************************************************************************ */
+  closedStream(eve) { }
   /************************************************************************************************************************************************************************ */
 
-
-
-  openedStream(eve, datainput: MatInput) {
-
-    // if (!this.obs) {
-    //   this.obs = new Subscription();
-    // }
-    // setTimeout((datainput) => {
-
-    //   Array.from(document.getElementsByTagName("mat-calendar"), (element => {
-    //     let instanceMatCalendar = window.ng.getComponent(element) as MatCalendar<Date>;
-    //     this.obs.add(instanceMatCalendar.monthView.selectedChange.subscribe(response => { this.stopOutFocus(datainput) }));
-    //     ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.addEventListener("touchmove", this.stopOutFocus.bind(this, datainput));
-    //     ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.addEventListener("mouseenter", this.stopOutFocus.bind(this, datainput));
-    //     ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.addEventListener("focus", this.stopOutFocus.bind(this, datainput));
-    //     ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.addEventListener("mouseleave", this.stopOutFocus.bind(this, datainput));
-    //     ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.addEventListener("blur", this.stopOutFocus.bind(this, datainput));
-    //     ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.addEventListener("mousedown", this.stopOutFocus.bind(this, datainput));
-    //     ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.addEventListener("touchstart", this.stopOutFocus.bind(this, datainput));
-    //     this.obs.add(instanceMatCalendar.monthView.dragStarted.subscribe(response => { this.stopOutFocus(datainput); }));
-    //     this.obs.add(instanceMatCalendar.monthView.dragEnded.subscribe(response => { this.stopOutFocus(datainput); }));
-    //   }))
-    // }, 110, datainput);
-  }
-  /************************************************************************************************************************************************************************ */
-  closedStream(eve) {
-    // Array.from(document.getElementsByTagName("mat-calendar"), (element => {
-    //   let instanceMatCalendar = window.ng.getComponent(element) as MatCalendar<Date>;
-    //   ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.removeEventListener("touchmove", this.stopOutFocus);
-    //   ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.removeEventListener("mouseenter", this.stopOutFocus);
-    //   ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.removeEventListener("focus", this.stopOutFocus);
-    //   ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.removeEventListener("mouseleave", this.stopOutFocus);
-    //   ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.removeEventListener("blur", this.stopOutFocus);
-    //   ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.removeEventListener("mousedown", this.stopOutFocus);
-    //   ((instanceMatCalendar.monthView._matCalendarBody as any)._elementRef).nativeElement.removeEventListener("touchstart", this.stopOutFocus);
-    // }))
-
-    // this.obs.unsubscribe();
-  }
   @Delay(0)
   stopOutFocus(datainput) {
-    datainput.focus()
   }
 
   constructor(protected override injector: Injector, protected override element: ElementRef) {
     super(injector, element);
-
   }
   /************************************************************************************************************************************************************************ */
 
